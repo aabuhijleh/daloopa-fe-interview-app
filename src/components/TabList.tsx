7ac2409e29e0f58cf8324464a00c11ddd584e808
Abdurrahman Abu-Hijleh <abdurrahman.abu.hijleh@ninjavan.co>
2024-08-07 14:11:17 +0300

    focus using arrows

--- src/components/TabList.tsx
+++ src/components/TabList.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import "./TabList.css";
 import { TabProps } from "./Tab";
 
@@ -13,22 +13,89 @@ export const TabList = ({
   selectedTabIdx,
   onSelectedTabChange,
 }: TabListProps) => {
+  useEffect(() => {
+    const tabList = document.querySelector('[role="tablist"]');
+    const tabs = tabList?.querySelectorAll(':scope > [role="tab"]');
+
+    let changeFocus: (event: Event) => void;
+
+    if (tabs) {
+      tabs.forEach((tab) => {
+        tab.addEventListener("click", changeTabs);
+      });
+
+      let tabFocus = 0;
+
+      changeFocus = (event: Event) => {
+        {
+          const e = event as KeyboardEvent;
+          if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+            tabs[tabFocus].setAttribute("tabindex", "-1");
+
+            if (e.key === "ArrowRight") {
+              tabFocus++;
+              if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+              }
+            } else if (e.key === "ArrowLeft") {
+              tabFocus--;
+              if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+              }
+            }
+
+            tabs[tabFocus].setAttribute("tabindex", "0");
+            (tabs[tabFocus] as HTMLElement).focus();
+          }
+        }
+      };
+
+      tabList?.addEventListener("keydown", changeFocus);
+    }
+
+    return () => {
+      tabs?.forEach((tab) => {
+        tab.removeEventListener("click", changeTabs);
+      });
+      tabList?.removeEventListener("keydown", changeFocus);
+    };
+  }, [selectedTabIdx, onSelectedTabChange]);
+
+  const changeTabs = (e: Event) => {
+    const targetTab = e.target as HTMLElement;
+    const tabList = targetTab.parentNode as HTMLElement;
+    const tabGroup = tabList.parentNode as HTMLElement;
+
+    tabList
+      .querySelectorAll(':scope > [aria-selected="true"]')
+      .forEach((t) => t.setAttribute("aria-selected", "false"));
+
+    targetTab.setAttribute("aria-selected", "true");
+
+    tabGroup
+      .querySelectorAll(':scope > [role="tabpanel"]')
+      .forEach((p) => p.setAttribute("hidden", "true"));
+
+    tabGroup
+      .querySelector(`#${targetTab.getAttribute("aria-controls")}`)
+      ?.removeAttribute("hidden");
+  };
+
   return (
     <div>
-      <div className="tab-list" role="tablist">
+      <div className="tab-list" role="tablist" aria-label="Tabs">
         {children.map((child, tabIdx) => {
           const isSelected = tabIdx === selectedTabIdx;
-          let className = "tab-item";
-          if (isSelected) {
-            className += " selected";
-          }
           return (
             <button
               key={tabIdx}
               role="tab"
               aria-selected={isSelected}
-              className={className}
+              aria-controls={`tab-panel-${tabIdx}`}
+              id={`tab-${tabIdx}`}
+              className={`tab-item ${isSelected ? "selected" : ""}`}
               onClick={() => onSelectedTabChange(tabIdx)}
+              tabIndex={isSelected ? 0 : -1}
             >
               <h2>{child.props.title}</h2>
             </button>
@@ -36,7 +103,21 @@ export const TabList = ({
         })}
       </div>
 
-      <div className="tab-content">{children[selectedTabIdx]}</div>
+      {children.map((child, tabIdx) => {
+        const isSelected = tabIdx === selectedTabIdx;
+        return (
+          <div
+            key={tabIdx}
+            id={`tab-panel-${tabIdx}`}
+            role="tabpanel"
+            aria-labelledby={`tab-${tabIdx}`}
+            hidden={!isSelected}
+            tabIndex={isSelected ? 0 : -1}
+          >
+            {child}
+          </div>
+        );
+      })}
     </div>
   );
 };
